e48850c84e11b3efc966f36be102b6437553abf9
krishnaguragain <mailkrishnaguragai@gmail.com>
2014-11-10 09:03:51 +0545

    Hello World Created
    ::Hello World

---
+++ 1.Hello-World/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main() {
+	printf("Hello World.\n");
+	getch();
+}
+
